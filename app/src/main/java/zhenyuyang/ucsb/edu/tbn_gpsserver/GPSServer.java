3ddd4601ff9f272e27f0de8356c738e9e7934641
Zhenyu Yang <zcbmlijygrdwa@ymail.com>
2017-06-18 15:59:36 -0700

    Initial commit

---
+++ app/src/main/java/zhenyuyang/ucsb/edu/tbn_gpsserver/GPSServer.java
@@ -0,0 +1,162 @@
+package zhenyuyang.ucsb.edu.tbn_gpsserver;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+/**
+ * Created by Zhenyu on 2017-06-18.
+ */
+
+public class GPSServer {
+    MainActivity activity;
+    ServerSocket serverSocket;
+    String message = "";
+    static final int socketServerPORT = 8080;
+    int count = 0;
+	public GPSServer(MainActivity activity) {
+        this.activity = activity;
+        Thread socketServerThread = new Thread(new SocketServerThread());
+        socketServerThread.start();
+    }
+
+    public int getPort() {
+        return socketServerPORT;
+    }
+
+    public void onDestroy() {
+        if (serverSocket != null) {
+            try {
+                serverSocket.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private class SocketServerThread extends Thread {
+
+
+
+        @Override
+        public void run() {
+            try {
+                serverSocket = new ServerSocket(socketServerPORT);
+
+                while (true) {
+                    Socket socket = serverSocket.accept();
+                    count++;
+                    message += "#" + count + " from "
+                            + socket.getInetAddress() + ":"
+                            + socket.getPort() + "\n";
+
+                    activity.runOnUiThread(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            activity.msg.setText(message);
+                        }
+                    });
+
+                    SocketServerReplyThread socketServerReplyThread = new SocketServerReplyThread(
+                            socket, count);
+                    socketServerReplyThread.run();
+
+                }
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    private class SocketServerReplyThread extends Thread {
+
+        private Socket hostThreadSocket;
+        int cnt;
+
+        SocketServerReplyThread(Socket socket, int c) {
+            hostThreadSocket = socket;
+            cnt = c;
+        }
+
+        @Override
+        public void run() {
+            OutputStream outputStream;
+            String msgReply = "Hello from Server, you are #" + cnt+"\n You lucky number today is "+Math.random();
+
+            try {
+                outputStream = hostThreadSocket.getOutputStream();
+                PrintStream printStream = new PrintStream(outputStream);
+                printStream.print(msgReply);
+                printStream.close();
+
+                message += "replayed: " + msgReply + "\n";
+
+                activity.runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        activity.msg.setText(message);
+                    }
+                });
+
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                message += "Something wrong! " + e.toString() + "\n";
+            }
+
+            activity.runOnUiThread(new Runnable() {
+
+                @Override
+                public void run() {
+                    activity.msg.setText(message);
+                }
+            });
+        }
+
+    }
+
+    public String getIpAddress() {
+        String ip = "";
+        try {
+            Enumeration<NetworkInterface> enumNetworkInterfaces = NetworkInterface
+                    .getNetworkInterfaces();
+            while (enumNetworkInterfaces.hasMoreElements()) {
+                NetworkInterface networkInterface = enumNetworkInterfaces
+                        .nextElement();
+                Enumeration<InetAddress> enumInetAddress = networkInterface
+                        .getInetAddresses();
+                while (enumInetAddress.hasMoreElements()) {
+                    InetAddress inetAddress = enumInetAddress
+                            .nextElement();
+
+                    if (inetAddress.isSiteLocalAddress()) {
+                        ip += "Server running at : "
+                                + inetAddress.getHostAddress();
+                    }
+                }
+            }
+
+        } catch (SocketException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            ip += "Something Wrong! " + e.toString() + "\n";
+        }
+        return ip;
+    }
+
+    public void reset(){
+        count = 0;
+        message = "";
+    }
+}
